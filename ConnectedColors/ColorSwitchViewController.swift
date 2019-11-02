57b8d90a526c4d0eacb09b10dd97cb35581f42ed
Ricardo Faure <ricardofaurejr@gmail.com>
2019-11-02 15:36:00 -0500

    Added JSON Decoding and Generation

--- ConnectedColors/ColorSwitchViewController.swift
+++ ConnectedColors/ColorSwitchViewController.swift
@@ -13,23 +13,25 @@ class ColorSwitchViewController: UIViewController {
         hermes.delegate = self
     }
 
-
-
     @IBOutlet weak var typed_text: UITextField!
     @IBOutlet weak var data_got: UILabel!
     @IBOutlet weak var room: UITextField!
     
     @IBAction func send(_ sender: Any) {
-        hermes.send(message: self.typed_text.text! + "%" + self.room.text!)
+        let json = "{\"messageData\": " + "\"" + typed_text.text + "\"" + ", \"roomName\": " + "\""  + room.text + "\"" + "}"
+        hermes.send(message: json)
     }
-    
-    
-    
-    
-    
 }
+//\"messageID\": " + String(id) + ",
+//, \"From\": " + source + "
 
 extension ColorSwitchViewController : HermesDelegate {
+    
+    struct Message: Codable {
+        let messageID: Int
+        let messageData: String
+        let roomName: String
+    }
 
     func connectedDevicesChanged(manager: Hermes, connectedDevices: [String]) {
         print("here")
@@ -41,17 +43,34 @@ extension ColorSwitchViewController : HermesDelegate {
     func sendMessage(manager: Hermes, colorString: String) {
         OperationQueue.main.addOperation {
 //            print("RECIEVED", self.room.text!, colorString.description.substring(from: colorString.description.range(of: "%")!.upperBound), self.room.text! == colorString.description.substring(from: colorString.description.range(of: "%")!.upperBound))
-            
+            /*
             if(self.room.text! == colorString.description.substring(from: colorString.description.range(of: "%")!.upperBound)) {
-                self.data_got.text! += colorString + "\n"
+                //self.data_got.text! += colorString + "\n"
+                
             }
+             */
+            self.decodeJSON(colorString: colorString)
             
+            
+            /*
             // if you have not already recieved the message, send it out again
             if(!self.all_messages.contains(555)){
                 self.hermes.send(message: colorString)
             }
+            */
             
         }
     }
-
+    
+    func decodeJSON(colorString: String) -> String{
+        guard let data = colorString.data(using: String.Encoding.utf8) else { fatalError("☠️") }
+        do {
+            let newMessage = try JSONDecoder().decode(Message.self, from: data)
+            print(newMessage)
+            return newMessage.messageData
+        } catch {
+            print(error)
+        }
+        return ""
+    }
 }
