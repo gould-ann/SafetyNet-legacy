8af3b2bc6db6fcbbdf82fd3ef0a1b5038d5d7e9a
Ann Gould <gould.ann8@gmail.com>
2019-11-01 22:21:54 -0500

    init commit

---
+++ Chat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chat
+//
+//  Created by Ann Gould on 11/1/19.
+//  Copyright © 2019 Ann Gould. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
