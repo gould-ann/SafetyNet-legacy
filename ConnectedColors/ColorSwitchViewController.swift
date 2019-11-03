95ab65c43e1f5a71ff6ef9359e8e314909bc4f7f
Ricardo Faure <ricardofaurejr@gmail.com>
2019-11-02 19:16:54 -0500

    temp

--- ConnectedColors/ColorSwitchViewController.swift
+++ ConnectedColors/ColorSwitchViewController.swift
@@ -26,11 +26,14 @@ class ColorSwitchViewController: UIViewController {
         let id = generateMessageID()
         let data: NSData = typed_text.text!.data(using: String.Encoding.utf8)! as NSData
         let zeroes: NSData = "00000".data(using: String.Encoding.utf8)! as NSData
-        let cipherText = RNCryptor.encrypt(data: data as Data, withPassword: chatroomID)
-        let cipherZero = RNCryptor.encrypt(data: zeroes as Data, withPassword: chatroomID)
+        //let cipherText = RNCryptor.encrypt(data: data as Data, withPassword: chatroomID)
+        //let cipherZero = RNCryptor.encrypt(data: zeroes as Data, withPassword: chatroomID)
+//        var json = "{\"messageID\":" + String(id)
+//        json += ", \"messageData\": \"" + String(decoding: cipherText, as: UTF8.self) + "\""
+//        json += ", \"flag\": \"" + String(decoding: cipherZero, as: UTF8.self) + "\"}"
         var json = "{\"messageID\":" + String(id)
-        json += ", \"messageData\": \"" + String(decoding: cipherText, as: UTF8.self) + "\""
-        json += ", \"flag\": \"" + String(decoding: cipherZero, as: UTF8.self) + "\"}"
+        json += ", \"messageData\": \"" + typed_text.text! + "\""
+        json += ", \"flag\": \"" + "00000" + "\"}"
         print(json)
         
         hermes.send(message: json)
@@ -84,22 +87,31 @@ extension ColorSwitchViewController : HermesDelegate {
                     chatroomID = self.room.text!
                 }
                 
-                let flag_data: NSData = message!.flag.data(using: String.Encoding.utf8)! as NSData
-                do {
-                    let decrypted_flag_data = try RNCryptor.decrypt(data: flag_data as Data, withPassword: chatroomID)
-                    let flag = String(decoding: decrypted_flag_data, as: UTF8.self)
-                    if(flag == "00000") {
-                        let message_data: NSData = message!.messageData.data(using: String.Encoding.utf8)! as NSData
-                        do {
-                            let decrypted_message_data = try RNCryptor.decrypt(data: message_data as Data, withPassword: chatroomID)
-                            let message = String(decoding: decrypted_message_data, as: UTF8.self)
-                            self.data_got.text! += message + "\n"
-                        } catch {
-                            print("Error Decrypting Message")
-                        }
-                    }
-                } catch {
-                    print("Error Decrypting Flag")
+//                let flag_data: NSData = message!.flag.data(using: String.Encoding.utf8)! as NSData
+//                do {
+//                    let decrypted_flag_data = try RNCryptor.decrypt(data: flag_data as Data, withPassword: chatroomID)
+//                    let flag = String(decoding: decrypted_flag_data, as: UTF8.self)
+//                    if(flag == "00000") {
+//                        let message_data: NSData = message!.messageData.data(using: String.Encoding.utf8)! as NSData
+//                        do {
+//                            let decrypted_message_data = try RNCryptor.decrypt(data: message_data as Data, withPassword: chatroomID)
+//                            let message = String(decoding: decrypted_message_data, as: UTF8.self)
+//                            self.data_got.text! += message + "\n"
+//                        } catch {
+//                            print("Error Decrypting Message")
+//                        }
+//                    }
+//                } catch {
+//                    print("Error Decrypting Flag")
+//                }
+                
+                if(message!.flag == "00000") {
+                //let message_data: NSData = message!.messageData.data(using: String.Encoding.utf8)! as NSData
+                //do {
+                    self.data_got.text! += message!.messageData + "\n"
+                //} catch {
+                  //  print("Error Decrypting Message")
+                //}
                 }
             }
             
